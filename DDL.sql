94e9336ce2bc0ab2654a7dfb3975be56f4889c56
EstefanyMM <47768454+EstefanyMM@users.noreply.github.com>
2020-04-09 15:11:37 -0600

    tabla Orden, DetalleOrden

---
+++ DDL.sql
@@ -0,0 +1,19 @@
+ALTER TABLE examen.Obra
+ADD CONSTRAINT FK_Proveedor
+FOREIGN KEY (IdProveedor)
+REFERENCES examen.Proveedor(IdProveedor)
+
+ALTER TABLE examen.Obra
+ADD CONSTRAINT FK_CategoriaObra
+FOREIGN KEY (IdCategoriaObra)
+REFERENCES examen.CategoriaObra(IdCategoriaObra)
+
+ALTER TABLE examen.DetalleOrden
+ADD CONSTRAINT FK_Obra
+FOREIGN KEY (IdObra)
+REFERENCES examen.Obra(IdObra)
+
+ALTER TABLE examen.DetalleOrden
+ADD CONSTRAINT FK_Orden
+FOREIGN KEY (IdOrden)
+REFERENCES examen.Orden(IdOrden)
