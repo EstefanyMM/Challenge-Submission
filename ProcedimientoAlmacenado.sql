96dd5a6ebc41506fbb33a061af84b03c650bc246
EstefanyMM <47768454+EstefanyMM@users.noreply.github.com>
2020-04-09 18:38:55 -0600

    Update ProcedimientoAlmacenado.sql

--- ProcedimientoAlmacenado.sql
+++ ProcedimientoAlmacenado.sql
@@ -12,3 +12,19 @@ DELETE FROM examen.CategoriaObra
 
 
 EXEC examen.clean_challenge
+
+
+ALTER PROCEDURE examen.clean_challenge
+AS
+
+DELETE FROM examen.DetalleOrden
+
+DELETE FROM examen.Obra
+DELETE FROM examen.Orden
+
+DELETE FROM examen.Cliente
+DELETE FROM examen.Proveedor
+DELETE FROM examen.CategoriaObra
+
+
+EXEC examen.clean_challenge
