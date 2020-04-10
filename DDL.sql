f45b6b7ed0f731b74322e8ae460b8e6d8b728403
EstefanyMM <47768454+EstefanyMM@users.noreply.github.com>
2020-04-09 18:28:10 -0600

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -39,14 +39,6 @@ REFERENCES examen.Cliente(IdCliente)
 ---------------------------------------------------------------------------------------------
 
 
-TRUNCATE TABLE examen.DetalleOrden
-
-DELETE FROM examen.Obra
-DELETE FROM examen.Orden
-
-DELETE FROM examen.Cliente
-DELETE FROM examen.Proveedor
-DELETE FROM examen.CategoriaObra 
 
 
 
